03bbce474eb3881c1102c8a030ca4c96a9a98b09
NumaanFormoli <numanformoliz@gmail.com>
2023-02-07 14:00:38 -0800

    Added test.sh and start.sh

---
+++ count-txts-non-fiction.sh
@@ -0,0 +1,3 @@
+find written_2/non-fiction > find-results-non-fiction.txt
+grep ".txt" find-results-non-fiction.txt > grep-results-non-fiction.txt
+wc grep-results-non-fiction.txt
---
+++ count-txts.sh
@@ -0,0 +1,4 @@
+find written_2 > find-results.txt
+grep ".txt" find-results.txt > grep-results.txt
+wc grep-results.txt
+
